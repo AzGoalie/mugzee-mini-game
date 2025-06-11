b6bebd40a633854a0d943907157e99fbd8406691
Travis McMahon <azgoalie@gmail.com>
2025-06-11 00:07:05 -0700

    Enabled top-level-await to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/mugzee-mini-game/",
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
 });
